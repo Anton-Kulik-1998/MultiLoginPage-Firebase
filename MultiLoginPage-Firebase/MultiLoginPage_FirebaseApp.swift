7a68079708a9cda007a21fd4e78943c9dd0b9422
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-26 14:41:04 +0300

    Initial Commit

---
+++ MultiLoginPage-Firebase/MultiLoginPage_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiLoginPage_FirebaseApp.swift
+//  MultiLoginPage-Firebase
+//
+//  Created by Антон Кулик on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiLoginPage_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
